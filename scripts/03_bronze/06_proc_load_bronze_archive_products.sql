da5ebbeb032f59cc3c4a5821173e2aeeb23a595e
Toyib Otusanya <toyibotusanya@gmail.com>
2025-11-25 13:15:46 +0000

    Update 06_proc_load_bronze_archive_products.sql

--- scripts/03_bronze/06_proc_load_bronze_archive_products.sql
+++ scripts/03_bronze/06_proc_load_bronze_archive_products.sql
@@ -42,8 +42,8 @@ BEGIN
 
 	BEGIN TRY
 		-- Retrieve corresponding batch_id from etl log table
-		SELECT @batch_id = etl_batch_id FROM audit.etl_log_table WHERE etl_layer = 'staging'
-		AND etl_table_loaded = 'archive_products';
+		SELECT TOP 1 @batch_id = etl_batch_id FROM audit.etl_log_table WHERE etl_layer = 'staging'
+		AND etl_table_loaded = 'archive_products' ORDER BY etl_start_time DESC;
 
 		-- Throw an error if corresponding batch_id is NULL
 		IF @batch_id IS NULL THROW 50001, 'bronze.archive_products cannot load before staging.archive_products. Load aborted.', 1;
