0b1e4f045a4e522c0680ca312d1ad425fede5d70
Toyib Otusanya <toyibotusanya@gmail.com>
2025-11-28 13:40:44 +0000

    Update 02_proc_load_bronze_archive_customers.sql

--- scripts/03_bronze/02_proc_load_bronze_archive_customers.sql
+++ scripts/03_bronze/02_proc_load_bronze_archive_customers.sql
@@ -13,10 +13,10 @@ Parameter: None.
 Usage: EXEC bronze.load_bronze_archive_customers;
 
 Note:
-	* Running this script assigns a unique batch_id to this table.
-	* Run the master procedure in the etl schema, as it performs a full ETL and 
-	  assigns a similar batch_id across all layers, and tables. This allows for 
-	  unified tracking, and thus enabling easy traceability and debugging.
+	* Running this retrieves run_id and source_batch_id from corresponding ingest tables.
+	* Run the master procedure in the etl schema, as it performs a full ETL and  assigns a 
+	  similar run_id across all tables, and layers. This allows for unified tracking, and 
+	  thus enabling easy traceability and debugging.
 ========================================================================================================
 */
 CREATE OR ALTER PROCEDURE bronze.load_bronze_archive_customers AS
@@ -24,9 +24,10 @@ BEGIN
 	-- Ensure transaction auto-aborts on severe errors
 	SET XACT_ABORT ON;
 	
-	-- Declare and map values to variables where necessary
+	-- Declare Variables
 	DECLARE 
-	@batch_id UNIQUEIDENTIFIER,
+	@run_id UNIQUEIDENTIFIER,
+	@source_batch_id UNIQUEIDENTIFIER,
 	@layer NVARCHAR(50) = 'bronze',
 	@table_loaded NVARCHAR(50) = 'archive_customers',
 	@proc_name NVARCHAR(50) = 'load_bronze_archive_customers',
@@ -41,13 +42,19 @@ BEGIN
 	@rows_diff INT;
 
 	BEGIN TRY
-		-- Retrieve corresponding batch_id from etl log table
-		SELECT TOP 1 @batch_id = etl_batch_id FROM audit.etl_log_table WHERE etl_layer = 'staging'
-		AND etl_table_loaded = 'archive_customers' ORDER BY etl_start_time DESC;
+		-- Retrieve corresponding run_id from etl log table
+		SELECT TOP 1 @run_id = etl_run_id FROM audit.etl_log_table 
+		WHERE etl_layer = 'staging' AND etl_table_loaded = 'archive_customers' 
+		ORDER BY etl_start_time DESC;
 
-		-- Throw an error if corresponding batch_id is NULL
-		IF @batch_id IS NULL THROW 50001, 
+		-- Throw an error if corresponding run_id is NULL
+		IF @run_id IS NULL THROW 50001, 
 		'bronze.archive_customers cannot load before staging.archive_customers. Load aborted.', 1;
+
+		-- Retrieve corresponding source_batch_id from etl log table
+		SELECT TOP 1 @source_batch_id = etl_source_batch_id FROM audit.etl_log_table
+		WHERE etl_layer = 'staging' and etl_table_loaded = 'archive_customers'
+		ORDER BY etl_start_time DESC;
 
 		-- Retrieve total rows from corresponding staging table
 		SELECT @rows_source = COUNT(*) FROM staging.archive_customers;
@@ -68,7 +75,7 @@ BEGIN
 		INSERT INTO bronze.archive_customers
 		(
 			customer_id,
-			name,
+			[name],
 			email,
 			country,
 			age,
@@ -80,13 +87,13 @@ BEGIN
 		)
 		SELECT 
 			customer_id,
-			name,
+			[name],
 			email,
 			country,
 			age,
 			signup_date,
 			marketing_opt_in,
-			@batch_id AS dwh_batch_id,
+			@source_batch_id AS dwh_batch_id,
 			CONCAT_WS('|', UPPER(CAST(customer_id AS VARCHAR(50))), UPPER(CAST(name AS VARCHAR(50))), 
 			UPPER(CAST(email AS VARCHAR(50))), UPPER(CAST(country AS VARCHAR(50))), UPPER(CAST(age AS VARCHAR(50))), 
 			UPPER(CAST(signup_date AS VARCHAR(50))), UPPER(CAST(marketing_opt_in AS VARCHAR(50)))) AS dwh_raw_rows,
@@ -111,7 +118,8 @@ BEGIN
 		-- Load etl log table
 		INSERT INTO audit.etl_log_table
 		(
-			etl_batch_id,
+			etl_run_id,
+			etl_source_batch_id,
 			etl_layer, 
 			etl_load_type, 
 			etl_proc_name,
@@ -127,7 +135,8 @@ BEGIN
 		)
 		VALUES
 		(
-			@batch_id,
+			@run_id,
+			@source_batch_id,
 			@layer,
 			@load_type,
 			@proc_name,
@@ -145,7 +154,8 @@ BEGIN
 
 	BEGIN CATCH
 		-- Map a default value to batch_id if NULL
-		IF @batch_id IS NULL SET @batch_id = '00000000-0000-0000-0000-000000000000';
+		IF @run_id IS NULL SET @run_id = '00000000-0000-0000-0000-000000000000';
+		IF @source_batch_id IS NULL SET @source_batch_id = '00000000-0000-0000-0000-000000000000';
 
 		-- Map a default value to start time if error occurs before ETL transaction
 		IF @start_time IS NULL SET @start_time = GETDATE();
@@ -157,16 +167,19 @@ BEGIN
 
 		-- Rollback any open transaction
 		IF @@TRANCOUNT > 0 ROLLBACK TRAN;
-
+		
+		-- Map value to rows source if NULL
+		IF @rows_source IS NULL SET @rows_source = 0;
 		-- Map value to rows loaded if NULL
 		IF @rows_loaded IS NULL SET @rows_loaded = 0;
-		-- Map value to row difference when an error occurs 
+		-- Map value to row difference when error occurs 
 		SET @rows_diff = @rows_source - @rows_loaded;
 
-		-- Load etl log table with error details
+		-- Load log table with error details
 		INSERT INTO audit.etl_log_table
 		(
-			etl_batch_id,
+			etl_run_id,
+			etl_source_batch_id,
 			etl_layer,
 			etl_table_loaded,
 			etl_proc_name,
@@ -188,7 +201,8 @@ BEGIN
 		)
 		VALUES
 		(
-			@batch_id,
+			@run_id,
+			@source_batch_id,
 			@layer,
 			@table_loaded,
 			@proc_name,
